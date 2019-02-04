7ccaacaba69ebfc28509ba212195d593eb86301e
Andrew Dunkman <andrew.dunkman@gsa.gov>
2019-02-04 12:28:06 -0500

    Fix current item highlight issue.
    This commit rewrites the navigation helpers to better handle selecting current items (adding .usa-current) around collections:
    
    - In the main navigation, items should be .usa-current when any member of the same collection is the active page.
    - In the side navigation, items should be .usa-current only when they are the current page.

--- docs/_plugins/uswds.rb
+++ docs/_plugins/uswds.rb
@@ -14,6 +14,27 @@ module USWDS
     a == b
   end
 
+  # Output the value if the page passed is the current page being rendered:
+  #
+  # {{ 'thing' | if_is_current_page: page_variable }}
+  # {{ 'thing' | if_is_current_page: page_variable, true }}
+  def if_is_current_page(value, page, current_if_in_collection)
+    current_page = @context['page']
+
+    if current_page['url'] == page['url']
+      value
+    elsif current_if_in_collection && !current_page['collection'].nil? && current_page['collection'] == page['collection']
+      value
+    end
+  end
+
+  # Check if a string starts with another string:
+  #
+  # {{ '#link-to-anchor' | starts_with: '#' }}
+  def starts_with(value, string)
+    value.start_with?(string)
+  end
+
   # Resolve a permalink URI into a page or collection document:
   #
   # {{ '/' | resolve_permalink %} => home page
