3fe40c7d85a462f7035244f310be3f081c9034a9
Karla Rodriguez <karla.rodriguez@gsa.gov>
2020-01-13 21:22:11 -0500

    Adding styles and scripts to dropdown action button

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@ const components = require('uswds/src/js/components');
 
 // login.gov additions
 components.accordionCloseButton = require('./components/accordionCloseButton');
+components.i18ndropdown = require('./components/dropdownButton');
 components.inputPasswordMeter = require('./components/inputPasswordStrength');
 components.invokeSpinner = require('./components/invokeSpinner');
 
