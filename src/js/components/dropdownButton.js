b1b38e10d3c710c849c756b6beeb97d458130fa4
Karla Rodriguez <karla.rodriguez@gsa.gov>
2020-01-14 18:45:43 -0500

    Circle ci indentations

--- src/js/components/dropdownButton.js
+++ src/js/components/dropdownButton.js
@@ -6,30 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const desktopDropdown = document.querySelector('.desktop-dropdown');
 
   function addListenerMulti(el, s, fn) {
-  s.split(' ').forEach(e => el.addEventListener(e, fn, false));
+    s.split(' ').forEach(e => el.addEventListener(e, fn, false));
   }
 
   function toggleAriaExpanded(element) {
-  if (element.getAttribute('aria-expanded') === 'true') {
-  element.setAttribute('aria-expanded', 'false');
-  } else {
-  element.setAttribute('aria-expanded', 'true');
-  }
+    if (element.getAttribute('aria-expanded') === 'true') {
+    element.setAttribute('aria-expanded', 'false');
+    } else {
+    element.setAttribute('aria-expanded', 'true');
+    }
   }
 
   function dropdownPicker(trigger, dropdown) {
-  addListenerMulti(trigger, 'click keypress', function (event) {
-  const eventType = event.type;
+    addListenerMulti(trigger, 'click keypress', function (event) {
+    const eventType = event.type;
 
-  event.preventDefault();
-  if (eventType === 'click' || (eventType === 'keypress' && event.which === 13)) {
-  this.parentNode.classList.toggle('focused');
-  dropdown.classList.toggle('display-none');
-  toggleAriaExpanded(this);
-  }
-  });
+    event.preventDefault();
+    if (eventType === 'click' || (eventType === 'keypress' && event.which === 13)) {
+    this.parentNode.classList.toggle('focused');
+    dropdown.classList.toggle('display-none');
+    toggleAriaExpanded(this);
+    }
+    });
   }
 
   if (desktopLink) dropdownPicker(desktopLink, desktopDropdown);
   if (mobileLink) dropdownPicker(mobileLink, mobileDropdown);
-  });
+    });
