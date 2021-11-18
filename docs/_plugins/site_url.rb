f8a010d8767070a73a08811f60c28b8e6a26c25b
Andrew Duthie <andrew.duthie@gsa.gov>
2021-11-18 15:08:34 -0500

    Use live site URL in production build
    Infer "make build" as intended for production consumption

--- docs/_plugins/site_url.rb
+++ docs/_plugins/site_url.rb
@@ -1,7 +1,7 @@
 FEDERALIST_PREVIEW_BRANCH_PREFIX = '/preview/'
 
 Jekyll::Hooks.register :site, :after_init do |site|
-  if ENV['BRANCH'].nil?
+  if ENV['BRANCH'].nil? && ENV['JEKYLL_ENV'] != 'production'
     site.config['url'] = "http://localhost:#{site.config['port']}"
   elsif ENV['BASEURL']&.start_with?(FEDERALIST_PREVIEW_BRANCH_PREFIX)
     # We don't have enough information to reconstruct the URL for the preview site. Setting to `nil`
