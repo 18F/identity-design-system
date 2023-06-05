1a488efdb6ff4ca1f0a05873440d60be93ec8c5e
Andrew Duthie <andrew.duthie@gsa.gov>
2023-06-05 15:41:31 -0400

    Remove most content from documentation output (#358)
    * Remove most content from documentation output
    
    * Replace other instance of getURLsFromSitemap

---
+++ docs/_components/index.md
@@ -0,0 +1,4 @@
+---
+permalink: /
+title: Components
+---
--- docs/_plugins/generate_nav_for_components.rb
+++ docs/_plugins/generate_nav_for_components.rb
@@ -2,8 +2,8 @@ require 'json'
 
 Jekyll::Hooks.register :site, :post_read do |site|
   site.collections.each do |name, collection|
-    site.data['nav'][name] = collection.docs.map do |doc|
-      { 'href' => doc.url }
-    end
+    site.data['nav'] = collection.docs.
+      filter { |doc| doc.basename != 'index.md'}.
+      map { |doc| { 'href' => doc.url } }
   end
 end
