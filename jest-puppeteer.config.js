1763d338c9c462788bd41c2ba3563f37e8da1bb7
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-03 15:52:27 -0400

    Use bundler to run Ruby
    Maybe needed for it to pick up webrick installed via bundler?

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@ process.env.JEST_PORT = process.env.JEST_PORT || '40410';
 
 module.exports = {
   server: {
-    command: `ruby -run -ehttpd ./dist -p${process.env.JEST_PORT}`,
+    command: `bundle exec ruby -run -ehttpd ./dist -p${process.env.JEST_PORT}`,
     port: parseInt(process.env.JEST_PORT, 10),
   },
 };
