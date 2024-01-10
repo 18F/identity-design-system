471eb321d832206c1ec15c1e56cc5f99990082e1
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-01-10 11:13:11 -0500

    Add concurrency to browser integration tests (#389)

--- test/accessibility.test.mjs
+++ test/accessibility.test.mjs
@@ -1,12 +1,16 @@
 /* eslint-disable no-await-in-loop */
 
-import { describe, before, after, test } from 'node:test';
+import { describe, before, after, test, it } from 'node:test';
 import assert from 'node:assert';
 import { createRequire } from 'node:module';
+import { relative, dirname } from 'node:path';
 import * as esbuild from 'esbuild';
+import glob from 'fast-glob';
 import puppeteer from 'puppeteer';
 
 const require = createRequire(import.meta.url);
+
+const paths = glob.sync('dist/*/index.html').map((path) => `/${dirname(relative('dist', path))}/`);
 
 const MESSAGE_TYPE_ERROR = 1;
 
@@ -20,52 +24,43 @@ describe('accessibility', () => {
   /** @type {import('puppeteer').Browser} */
   let browser;
 
-  /** @type {import('puppeteer').Page} */
-  let page;
-
   before(async () => {
     esbuildContext = await esbuild.context({});
     port = (await esbuildContext.serve({ servedir: 'dist' })).port;
     browser = await puppeteer.launch({ headless: 'new' });
-    page = await browser.newPage();
   });
 
   after(async () => {
     await Promise.all([browser.close(), esbuildContext.dispose()]);
   });
 
-  test('passes automated accessibility test', async (t) => {
-    await page.goto(`http://localhost:${port}`);
-    const urls = await page.$$eval('.usa-nav__link', (links) => links.map((link) => link.href));
+  it('has pages to test', () => {
+    assert(paths.length);
+  });
 
-    assert(urls.length);
-
-    await Promise.all(
-      urls.map((url) =>
-        t.test(url, async () => {
-          await page.goto(url);
-          await page.addScriptTag({
-            path: require.resolve('html_codesniffer/build/HTMLCS.js'),
-          });
-          const messages = await page.evaluate(
-            () =>
-              new Promise((resolve, reject) => {
-                window.HTMLCS.process('WCAG2AA', window.document, (error) => {
-                  if (error) {
-                    reject(error);
-                  } else {
-                    resolve(window.HTMLCS.getMessages());
-                  }
-                });
-              }),
-          );
-          for (const message of messages) {
-            if (message.type === MESSAGE_TYPE_ERROR) {
-              throw message;
-            }
-          }
-        }),
-      ),
-    );
+  paths.forEach((path) => {
+    test(path, async () => {
+      const page = await browser.newPage();
+      await page.goto(`http://localhost:${port}${path}`);
+      await page.addScriptTag({ path: require.resolve('html_codesniffer/build/HTMLCS.js') });
+      const messages = await page.evaluate(
+        () =>
+          new Promise((resolve, reject) => {
+            window.HTMLCS.process('WCAG2AA', window.document, (error) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(window.HTMLCS.getMessages());
+              }
+            });
+          }),
+      );
+      for (const message of messages) {
+        if (message.type === MESSAGE_TYPE_ERROR) {
+          throw message;
+        }
+      }
+      await page.close();
+    });
   });
 });
