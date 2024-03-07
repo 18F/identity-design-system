9e0fdf5cd2c9f803926dd5d5afce58e6f0cf0bb9
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-03-07 08:41:04 -0500

    Add header documentation with mobile preview (#418)
    * Add header documentation with mobile preview
    
    * Remove stray newline
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>
    
    * Use static embed markup for mobile preview
    
    * Re-include init in component layout
    
    * Remove unused load_script option in base layout
    
    * Reuse common header and footer in layouts
    
    Avoid inconsistencies causing issues like what was fixed in c9d9eb2
    
    * Avoid extra newline
    
    ---------
    
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- docs/_plugins/generate_nav_for_components.rb
+++ docs/_plugins/generate_nav_for_components.rb
@@ -1,9 +1,7 @@
 require 'json'
 
 Jekyll::Hooks.register :site, :post_read do |site|
-  site.collections.each do |name, collection|
-    site.data['nav'] = collection.docs.
-      filter { |doc| doc.basename != 'index.md'}.
-      map { |doc| { 'href' => doc.url } }
-  end
+  site.data['nav'] = site.collections['components'].docs.
+    filter { |doc| doc.basename != 'index.md'}.
+    map { |doc| { 'href' => doc.url } }
 end
