e18b3fd8e40d98c9cb968f29c6b615e36e501080
sudheer.dandamudi <sudheer.dandamudi@gsa.gov>
2024-05-30 14:31:24 +0000

    Update init.test.js

--- test/init.test.js
+++ test/init.test.js
@@ -20,7 +20,7 @@ describe('initializer script', () => {
   before(async () => {
     esbuildContext = await esbuild.context({});
     port = (await esbuildContext.serve({ servedir: 'dist' })).port;
-    browser = await puppeteer.launch({args: ['--no-sandbox'],}); 
+    browser = await puppeteer.launch({ args: ['--no-sandbox'], }); 
     originalPageMarkup = await readFile('dist/accordions/index.html', 'utf-8');
     await writeFile(
       'dist/accordions/index.html',
