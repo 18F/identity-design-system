c9addc0c429d46381eaded8da26d57a5ef6494aa
lucas.dze <lucas.kouongadze@gsa.gov>
2024-06-04 14:37:19 +0000

    Update screenshot.test.js

--- test/screenshot.test.js
+++ test/screenshot.test.js
@@ -13,12 +13,12 @@ const exec = promisify(_exec);
 
 const branch = (await exec('git branch --show-current')).stdout.trim();
 
+const DIFF_DIRECTORY = 'tmp/screenshot/diff';
 const SNAPSHOT_DIRECTORY = 'tmp/screenshot/branches';
-const MAIN_SNAPSHOTS_DIRECTORY = join(SNAPSHOT_DIRECTORY, 'migation-to-gl');
+const MAIN_SNAPSHOTS_DIRECTORY = join(SNAPSHOT_DIRECTORY, 'main');
 const BRANCH_SNAPSHOTS_DIRECTORY = join(SNAPSHOT_DIRECTORY, branch);
 
 const skip = !!process.env.SKIP_VISUAL_REGRESSION_TEST;
-
 
 describe('screenshot visual regression', { skip, concurrency: true }, async () => {
   /** @type {string[]} */
