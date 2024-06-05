db729cbf95da244f2bd6a0863163684f7a8071fc
dskgsa <sudheer.dandamudi@gsa.gov>
2024-06-05 09:02:03 -0400

    update dependencies

--- test/screenshot.test.js
+++ test/screenshot.test.js
@@ -14,7 +14,7 @@ const branch = (await exec('git branch --show-current')).stdout.trim();
 const DIFF_DIRECTORY = 'tmp/screenshot/diff';
 const SNAPSHOT_DIRECTORY = 'tmp/screenshot/branches';
 const MAIN_SNAPSHOTS_DIRECTORY = join(SNAPSHOT_DIRECTORY, 'main');
-const BRANCH_SNAPSHOTS_DIRECTORY = join(SNAPSHOT_DIRECTORY, branch);
+const BRANCH_SNAPSHOTS_DIRECTORY = 'tmp/screenshot/branches';
 
 const skip = !!process.env.SKIP_VISUAL_REGRESSION_TEST;
 
