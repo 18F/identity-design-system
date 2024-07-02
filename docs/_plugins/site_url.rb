9f1a12b56a1e3c0a200d50dbbb3f19c69ed84c6c
Andrew Duthie <andrew.duthie@gsa.gov>
2024-07-02 17:02:21 -0400

    Try adding preview deploy

--- docs/_plugins/site_url.rb
+++ docs/_plugins/site_url.rb
@@ -1,12 +1,9 @@
-CLOUD_GOV_PAGES_PREVIEW_BRANCH_PREFIX = '/preview/'
-
 Jekyll::Hooks.register :site, :after_init do |site|
-  if ENV['BRANCH'].nil? && ENV['JEKYLL_ENV'] != 'production'
+  site.config['baseurl'] = ENV['PREVIEW_BASEURL'] if ENV['PREVIEW_BASEURL']
+
+  if ENV['PREVIEW_URL']
+    site.config['url'] = ENV['PREVIEW_URL']
+  elsif ENV['JEKYLL_ENV'] != 'production'
     site.config['url'] = "http://localhost:#{site.config['port']}"
-  elsif ENV['BASEURL']&.start_with?(CLOUD_GOV_PAGES_PREVIEW_BRANCH_PREFIX)
-    # We don't have enough information to reconstruct the URL for the preview site. Setting to `nil`
-    # at least allows redirects to work correctly. Sitemap URLs will be invalid, but since preview
-    # sites aren't intended to be indexed, this is tolerable.
-    site.config['url'] = nil
   end
 end
