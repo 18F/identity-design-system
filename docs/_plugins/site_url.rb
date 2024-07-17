ffc647e9de625a2875dea1ae2d9e938ca341b911
Andrew Duthie <andrew.duthie@gsa.gov>
2024-07-17 14:28:49 -0400

    Readd baseurl configuration

--- docs/_plugins/site_url.rb
+++ docs/_plugins/site_url.rb
@@ -1,4 +1,6 @@
 Jekyll::Hooks.register :site, :after_init do |site|
+  site.config['baseurl'] = ENV['PREVIEW_BASEURL'] if ENV['PREVIEW_BASEURL']
+
   if ENV['PREVIEW_URL']
     site.config['url'] = ENV['PREVIEW_URL']
   elsif ENV['JEKYLL_ENV'] != 'production'
