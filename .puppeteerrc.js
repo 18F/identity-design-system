d916dc9f9b470e88cc5cfeafbdaf12ca543db8ec
AJ Farkas <aj.farkas@gsa.gov>
2026-07-21 16:04:44 -0400

    use Common JS for puppeteer config

--- .puppeteerrc.js
+++
@@ -1,9 +0,0 @@
-import { join } from 'path';
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-export default {
-  // changes cache location Puppeteer
-  cacheDirectory: join(import.meta.dirname, '.cache', 'puppeteer'),
-};
